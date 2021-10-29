023269176c56acb2b4028a9ce0241ddfc08a0228
YUTA UEHARA <mame5124@gmail.com>
2021-10-29 16:47:37 +0900

    ## TaskEX

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-  extend Devise::Models # 追加
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true
+
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
